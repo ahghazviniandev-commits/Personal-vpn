dd3a1fe1ea86b1580ccc4e1ffe6bc22e6344b11a
ahghazviniandev-commits <ah.ghazviniandev@gmail.com>
2026-06-21 15:41:38 +0330

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
 clear
+
 echo "   ___         _    ___  ___ ____   "
 echo "  / __|___ _ _| |_ / _ \/ __|__  |  "
 echo " | (__/ -_) ' \  _| (_) \__ \ / /   "
 echo "  \___\___|_||_\__|\___/|___//_/    "
-echo " OpenVPN Access Server With Unlimited License"
+echo " Backup Sender to Telegram"
 
-yum update -y
-yum install net-tools -y
-rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
-rm /usr/local/openvpn_as/lib/python2.7/site-packages/pyovpn-2.0-py2.7.egg
-cp pyovpn-2.0-py2.7.egg /usr/local/openvpn_as/lib/python2.7/site-packages/
-cd /usr/local/openvpn_as/bin
-./ovpn-init
+# install dependencies (Ubuntu 22.04)
+sudo apt update -y
+sudo apt install -y zip curl cron
 
-
-echo "=========================DONE=============================="
-echo "SETUP ADMIN PASSWORD"
-
-passwd openvpn
+# ensure cron is running
+sudo systemctl enable cron
+sudo systemctl start cron
 
 
-echo "=========================DONE=============================="
+# Bot token
+while [[ -z "$tk" ]]; do
+    echo "Bot token:"
+    read -r tk
+    if [[ -z "$tk" ]]; then
+        echo "Invalid input. Token cannot be empty."
+    fi
+done
+
+
+# Chat id
+while [[ -z "$chatid" ]]; do
+    echo "Chat id:"
+    read -r chatid
+    if [[ -z "$chatid" ]]; then
+        echo "Invalid input. Chat id cannot be empty."
+    elif [[ ! $chatid =~ ^-?[0-9]+$ ]]; then
+        echo "${chatid} is not a number."
+        chatid=""
+    fi
+done
+
+
+# Caption
+echo "Caption (for example your domain):"
+read -r caption
